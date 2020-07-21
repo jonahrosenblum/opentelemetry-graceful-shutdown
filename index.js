35ca0739ff99d9137db3a66a075e349638543fbf
jonahrosenblum <jonaherosenblum@gmail.com>
2020-07-21 19:02:51 +0000

    test endpoint access again

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict'
 
 const express = require('express');
-const { BasicTracerProvider, BatchSpanProcessor } = require('@opentelemetry/tracing')
-const { TraceExporter } = require('@google-cloud/opentelemetry-cloud-trace-exporter')
+// const { BasicTracerProvider, BatchSpanProcessor } = require('@opentelemetry/tracing')
+// const { TraceExporter } = require('@google-cloud/opentelemetry-cloud-trace-exporter')
 
-const exporter = new TraceExporter();
-const provider = new BasicTracerProvider();
-provider.addSpanProcessor(new BatchSpanProcessor(exporter));
-const tracer = provider.getTracer();
+// const exporter = new TraceExporter();
+// const provider = new BasicTracerProvider();
+// provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+// const tracer = provider.getTracer();
 
 const app = express();
 
-app.get('/trace', (req, res) => {
-    tracer.startSpan('foo');
-    res.send(`started trace`);
-});
+// app.get('/trace', (req, res) => {
+//     tracer.startSpan('foo');
+//     res.send(`started trace`);
+// });
 
 app.get('/', (req, res) => {
     res.send(`home`);
