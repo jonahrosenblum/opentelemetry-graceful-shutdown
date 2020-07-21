1f3758065833d418b68c0812394cbc357d4040b2
jonahrosenblum <jonaherosenblum@gmail.com>
2020-07-21 15:32:29 +0000

    add tracing

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const {BasicTraceProvider, BatchSpanProcessor} = require('@opentelemetry/tracing')
+const {StackdriverTraceExporter} = require('@opentelemtry/exporter-stackdriver-trace')
+
+const exporter = StackdriverTraceExporter();
+const processor = BatchSpanProcessor(exporter);
+const provider = BasicTraceProvider();
+provider.addSpanProcessor(processor)
+const tracer = provider.getTracer();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`Hello World!`);
+app.get('/trace', (req, res) => {
+    tracer.startSpan('foo');
+    res.send(`started trace`);
 });
 
 const port = 8080;
